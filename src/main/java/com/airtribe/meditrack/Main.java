720c3bf58be9ce0e012cabf5401f9ed9ac9baefa
neelchavan <neelchavan9@gmail.com>
2026-05-11 21:20:08 +0530

    initial commit

---
+++ src/main/java/com/airtribe/meditrack/Main.java
@@ -0,0 +1,7 @@
+package main.java.com.airtribe.meditrack;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome back to MediTrack!");
+        // Initialize the application and start the user interface
+    }
+}
